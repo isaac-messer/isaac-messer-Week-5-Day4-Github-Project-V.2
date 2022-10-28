b5232ad0d71a4a415a24746976568cdd777cc883
Isaac Messer <isaac.messer640@gmail.com>
2022-10-27 21:31:56 -0600

    Final Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+//Navbar items
+    $('.navItem').css('color', 'white');
+    $('#nav').css('background-color', '#145');
+//list items
+    $('.even').css('background-color', '#ccc');
+    $('.odd').css('background-color', '#aaa');
+//Input Change
+     $('input').attr('placeholder', 'Isaac Messer');
+});
+    
